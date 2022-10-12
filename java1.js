b0cb97ef27885507e748b16acbd05dd2bfc6c1fa
Hamoudi0198 <95136279+Hamoudi0198@users.noreply.github.com>
2022-10-12 18:50:21 +0300

    commit message

---
+++ java1.js
@@ -0,0 +1,20 @@
+    function positions(){
+        document.getElementById("pacman").style.top ="12%"
+    }
+
+    function down (){
+        document.getElementById("pacman").style.top ="85%"
+    }
+
+    function left (){
+        document.getElementById("pacman").style.left ="12%"
+    }
+
+    function right(){
+        document.getElementById("pacman").style.left ="85%"
+    }
+
+    function reset(){
+        document.getElementById("pacman").style.left ="50%"
+        document.getElementById("pacman").style.top="50%"
+    }
